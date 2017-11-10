0dce932ff476ce52d781d1e220f68f586705cc9a
tyliu716 <louislty@msn.com>
2017-11-10 22:47:12 +0800

    Init.

---
+++ README.md
@@ -0,0 +1,44 @@
+# pm2-dingtalk
+
+This is a PM2 Module for sending events & logs from your PM2 processes to Dingtalk.
+
+## Install
+
+To install and setup pm2-dingtalk, run the following commands:
+
+```
+pm2 install pm2-dingtalk
+pm2 set pm2-dingtalk:dingtalk_url https://dingtalk_custom_robot_url
+```
+
+To get the Dingtalk URL, you need to setup an Incoming Webhook. More details on how to set this up can be found here: https://open-doc.dingtalk.com/docs/doc.htm?spm=a219a.7629140.0.0.karFPe&treeId=257&articleId=105735&docType=1
+
+## Configure
+
+The following events can be subscribed to:
+
+- log - All standard out logs from your processes. Default: false
+- error - All error logs from your processes. Default: true
+- kill - Event fired when PM2 is killed. Default: true
+- exception - Any exceptions from your processes. Default: true
+- restart - Event fired when a process is restarted. Default: false
+- delete - Event fired when a process is removed from PM2. Default: false
+- stop - Event fired when a process is stopped. Default: false
+- restart overlimit - Event fired when a process is reaches the max amount of times it can restart. Default: true
+- exit - Event fired when a process is exited. Default: false
+- start -  Event fired when a process is started. Default: false
+- online - Event fired when a process is online. Default: false
+
+You can simply turn these on and off by setting them to true or false using the PM2 set command.
+
+```
+pm2 set pm2-dingtalk:log true
+pm2 set pm2-dingtalk:error false
+```
+
+## Contributing
+
+## Release Historyf
+
+## Inspired by pm2-slack
+https://github.com/mattpker/pm2-slack
---
+++ index.js
@@ -0,0 +1,165 @@
+'use strict';
+
+var os = require('os');
+var pm2 = require('pm2');
+var pmx = require('pmx');
+var Urllib = require("urllib");
+
+// Get the configuration from PM2
+var conf = pmx.initModule();
+
+// Set the events that will trigger the color red
+var redEvents = ['stop', 'exit', 'delete', 'error', 'kill', 'exception', 'restart overlimit', 'suppressed'];
+
+// create the message queue
+var messages = [];
+
+// create the suppressed object for sending suppression messages
+var suppressed = {
+    isSuppressed: false,
+    date: new Date().getTime()
+};
+
+var moduelName = 'pm2-dingtalk';
+
+function sendDingtalk(message) {
+    var name = message.name;
+    var event = message.event;
+    var description = message.description;
+
+    // If a Dingtalk robot URL is not set, we do not want to continue and nofify the user that it needs to be set
+    if (!conf.dingtalk_url) return console.error("There is no Dingtalk URL set, please set the Dingtalk URL: 'pm2 set pm2-dingtalk:dingtalk_url https://dingtalk_url'");
+
+    // The JSON payload to send to the Webhook
+    var payload = {
+        "msgtype": "text",
+        "text": {
+            "content": 'PM2 Event Notify \n\n' + name + ' \n ' + event + ' \n ' + description
+        },
+        "at": {
+            "atMobiles": [],
+            "isAtAll": false
+        }
+    };
+
+    try {
+        Urllib.request(conf.dingtalk_url, {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            timeout: 60000,
+            data: payload
+        }, function (err, data, res) {
+            if (err) return console.error(err);
+            console.log(data.toString());
+            // {"errcode":0,"errmsg":"ok"}
+        });
+    } catch (error) {
+
+    }
+}
+
+// Function to process the message queue
+function processQueue() {
+
+    // If we have a message in the message queue, removed it from the queue and send it to dingtalk
+    if (messages.length > 0) {
+        sendDingtalk(messages.shift());
+    }
+
+    // If there are over 10 messages in the queue, send the suppression message if it has not been sent and delete all the messages in the queue after 10
+    if (messages.length > 10) {
+        if (!suppressed.isSuppressed) {
+            suppressed.isSuppressed = true;
+            suppressed.date = new Date().getTime();
+            sendDingtalk({
+                name: 'pm2-dingtalk',
+                event: 'suppressed',
+                description: 'Messages are being suppressed due to rate limiting.'
+            });
+        }
+        messages.splice(10, messages.length);
+    }
+
+    // If the suppression message has been sent over 1 minute ago, we need to reset it back to false
+    if (suppressed.isSuppressed && suppressed.date < (new Date().getTime() - 60000)) {
+        suppressed.isSuppressed = false;
+    }
+
+    // Wait 10 seconds and then process the next message in the queue
+    setTimeout(function () {
+        processQueue();
+    }, 10000);
+}
+
+// Start listening on the PM2 BUS
+pm2.launchBus(function (err, bus) {
+
+    // Listen for process logs
+    if (conf.log) {
+        bus.on('log:out', function (data) {
+            if (data.process.name !== moduelName) {
+                messages.push({
+                    name: data.process.name,
+                    event: 'log',
+                    description: JSON.stringify(data.data)
+                });
+            }
+        });
+    }
+
+    // Listen for process errors
+    if (conf.error) {
+        bus.on('log:err', function (data) {
+            if (data.process.name !== moduelName) {
+                messages.push({
+                    name: data.process.name,
+                    event: 'error',
+                    description: JSON.stringify(data.data)
+                });
+            }
+        });
+    }
+
+    // Listen for PM2 kill
+    if (conf.kill) {
+        bus.on('pm2:kill', function (data) {
+            messages.push({
+                name: 'PM2',
+                event: 'kill',
+                description: data.msg
+            });
+        });
+    }
+
+    // Listen for process exceptions
+    if (conf.exception) {
+        bus.on('process:exception', function (data) {
+            if (data.process.name !== moduelName) {
+                messages.push({
+                    name: data.process.name,
+                    event: 'exception',
+                    description: JSON.stringify(data.data)
+                });
+            }
+        });
+    }
+
+    // Listen for PM2 events
+    bus.on('process:event', function (data) {
+        if (conf[data.event]) {
+            if (data.process.name !== moduelName) {
+                messages.push({
+                    name: data.process.name,
+                    event: data.event,
+                    description: 'The following event has occured on the PM2 process ' + data.process.name + ': ' + data.event
+                });
+            }
+        }
+    });
+
+    // Start the message processing
+    processQueue();
+
+});
